8bd3ea069cf1b882a19854d02e4c46ca48f85865
arad4228 <arad4228@gmail.com>
2022-04-05 13:58:31 +0900

    백준 [1629] 곱셈(내용 수정)

--- BackJoon/Divdle&Conquer(분할정복)/1629/1629.cpp
+++ BackJoon/Divdle&Conquer(분할정복)/1629/1629.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
@@ -7,10 +6,13 @@ long long pow(int A, int B,int C)
 {
     if (B == 0)
         return 1;
+
     long long temp = pow(A,B/2,C);
     temp = temp * temp % C;
+    // 만약 B의 값이 짝수라면
     if (B%2 == 0)
         return temp;
+    // B의 값이 홀수라면 아래의 작업을 진행.
     else
         return temp * A %C;
 }
