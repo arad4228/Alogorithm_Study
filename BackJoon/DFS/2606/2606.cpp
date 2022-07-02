d08464c46fbfdee22a36b1054993c3de839bf4d1
arad4228 <arad4228@gmail.com>
2022-07-02 09:16:55 +0900

    백준 [2606] 바이러스(해설추가)

--- BackJoon/DFS/2606/2606.cpp
+++ BackJoon/DFS/2606/2606.cpp
@@ -2,10 +2,11 @@
 #include <vector>
 
 using namespace std;
-bool visited[100] = {false,};
-vector<int> virus[100];
+bool visited[101] = {false,};
+vector<int> virus[101];
 int cnt = 0;
 
+// dfs의 알고리즘
 void dfs(int v) 
 {
 	visited[v] = true;
@@ -15,6 +16,7 @@ void dfs(int v)
 		if (!visited[next])
         {   
             dfs(next);
+            // dfs를 진행하면서 노드의 갯수를 파악
             cnt++;
 
         }
@@ -23,7 +25,9 @@ void dfs(int v)
 
 int main(void)
 {
+    // 노드의 갯수
     int C;
+    // 간선의 갯수
     int N;
     cin >> C >> N;
     for(int i= 0; i < N; i++)
